93f94b9f05b74f2d28074045cf599ce21ac4c74e
Artur Saganowski <a.saganowski00@gmail.com>
2022-09-04 22:45:52 +0200

    deleting photos

--- src/components/images/photos.js
+++ src/components/images/photos.js
@@ -1,12 +1,3 @@
-import chest from "./chest.jpeg"
-import back from "./back.jpeg"
-import legs from "./legs2.png"
-import arms from "./arms.jpeg"
-import core from "./core.jpeg"
-import forearms from "./forearms.jpeg"
-import neck from "./neck.jpeg"
-import shoulders from "./shoulders.jpeg"
-import calves from "./calves.webp"
 import protein from "./highProtein.png"
 import carb from "./carb.jpeg"
 import fat from "./fat.png"
@@ -16,15 +7,6 @@ import sugar from "./sugar.jpeg"
 
 
 export{
-  chest,
-  back,
-  legs,
-  arms,
-  core,
-  neck,
-  shoulders,
-  forearms,
-  calves,
   protein,
   carb,
   fat,
